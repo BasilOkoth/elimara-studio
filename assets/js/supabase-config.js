0c722a294a71aac4628f1222c8fdf4400f30f547
Basil Okoth <162017549+BasilOkoth@users.noreply.github.com>
2026-08-31 22:58:07 +0300

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,5 +1,5 @@
 window.ELIMARA_SUPABASE_CONFIG = {
-  url: "",
-  anonKey: "",
+  url: "https://lvzofvofcqhrczhoorza.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2em9mdm9mY3FocmN6aG9vcnphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxOTk2NjgsImV4cCI6MjEwMzc3NTY2OH0.HNLS7fkQy305VgPWDfNtmRvTW70DkFvlyjw0u5EZywk",
   bucket: "studio-media"
 };
